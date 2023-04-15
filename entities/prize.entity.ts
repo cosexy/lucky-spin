84a92a775156c7fc34dd1f98792e7c7011fa0e0b
Guen <heloo@guen.dev>
2023-04-15 15:13:46 +0700

    cm

--- entities/prize.entity.ts
+++ entities/prize.entity.ts
@@ -10,4 +10,4 @@ export interface PrizeEntity {
 }
 
 
-export const prizes: string[] = ['80.000.000 VND', '50.000.000 VND', 'XÉ MÁY VISON', 'VÀNG MIẾNG SJC  9999', 'TÚI SÁCH LV', 'IPHONE 14 PRO', 'VUOCHER 2.000.000', 'NỒI CƠM ĐIỆN', 'VOUCHER 200.000', 'CHÚC BẠN MAY MẮN']
+export const prizes: string[] = ['80.000.000 VND', '50.000.000 VND', 'XÉ MÁY VISON', 'VÀNG MIẾNG SJC  9999', 'TÚI SÁCH LV', 'IPHONE 14 PRO', 'VUOCHER 2.000.000', 'NỒI CƠM ĐIỆN', 'VOUCHER 200.000', 'XE MÁY VÀNG', 'ĐIỆN THOẠI', 'CHÚC BẠN MAY MẮN']
